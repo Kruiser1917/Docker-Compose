9904457f6cb671c9649c91c2dfa8b679b6069d9f
aptik <aptik@DESKTOP-SVN8V0A>
2024-11-16 15:06:29 +0300

    Initial commit with models, views, and serializers

--- lms/urls.py
+++ lms/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),  # Эндпоинты для курсов
+    path('', include(router.urls)),
     path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
     path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
 ]
